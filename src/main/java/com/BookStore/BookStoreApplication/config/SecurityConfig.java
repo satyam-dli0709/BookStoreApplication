195f20432a779c9fa3d6f1e48281fe79b843597a
Ajaygattu781 <Ajay.Gattu@godigit.com>
2024-08-30 13:50:27 +0530

    Updated: JWT implemented in admin

--- src/main/java/com/BookStore/BookStoreApplication/config/SecurityConfig.java
+++ src/main/java/com/BookStore/BookStoreApplication/config/SecurityConfig.java
@@ -1,33 +1,79 @@
 package com.BookStore.BookStoreApplication.config;
 
 
+import com.BookStore.BookStoreApplication.filter.JwtRequestFilter;
+import com.BookStore.BookStoreApplication.service.AdminServiceImpl;
+import com.BookStore.BookStoreApplication.util.JwtUtil;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.aspectj.EnableSpringConfigured;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
 
 public class SecurityConfig {
-    @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        http
-                .csrf(AbstractHttpConfigurer::disable) // Disable CSRF protection
-                .authorizeHttpRequests(authorize -> authorize
-                        .anyRequest().permitAll() // Allow all requests without authentication
-                );
-        return http.build();
-    }
+//    @Bean
+//    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+//        http
+//                .csrf(AbstractHttpConfigurer::disable) // Disable CSRF protection
+//                .authorizeHttpRequests(authorize -> authorize
+//                        .anyRequest().permitAll() // Allow all requests without authentication
+//                );
+//        return http.build();
+//    }
 
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
+    }
+
+    @Autowired
+    private AdminServiceImpl adminService;
+
+    @Autowired
+    private JwtUtil jwtUtil;
+
+    @Autowired
+    private JwtRequestFilter jwtRequestFilter;
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http.csrf(csrf -> csrf.disable())
+                .authorizeHttpRequests(auth -> {
+                    auth.requestMatchers("/bookstore_user/admin/login", "/bookstore_user/admin/register").permitAll();
+                    auth.requestMatchers("/bookstore_user/admin/add/book", "/bookstore_user/admin/update/book/**", "/bookstore_user/admin/delete/book").authenticated();
+                    auth.anyRequest().permitAll();
+                })
+                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+        http.addFilterBefore(jwtRequestFilter, UsernamePasswordAuthenticationFilter.class);
+        return http.build();
+    }
+
+
+
+
+    @Bean
+    public WebSecurityCustomizer webSecurityCustomizer() {
+        return (web) -> web.ignoring().requestMatchers("/ignore1", "/ignore2");
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(HttpSecurity http) throws Exception {
+        AuthenticationManagerBuilder authenticationManagerBuilder = http.getSharedObject(AuthenticationManagerBuilder.class);
+        authenticationManagerBuilder.userDetailsService(adminService).passwordEncoder(passwordEncoder());
+        return authenticationManagerBuilder.build();
     }
 
 }
