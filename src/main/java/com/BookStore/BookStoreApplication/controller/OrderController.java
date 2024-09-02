dee20a260c488f99988cedbf4c5fede9c45bb2fb
Satyam Patle <Satyam.Patle@godigit.com>
2024-09-02 10:29:21 +0530

    update: logger

--- src/main/java/com/BookStore/BookStoreApplication/controller/OrderController.java
+++ src/main/java/com/BookStore/BookStoreApplication/controller/OrderController.java
@@ -3,10 +3,7 @@ package com.BookStore.BookStoreApplication.controller;
 import com.BookStore.BookStoreApplication.model.Order;
 import com.BookStore.BookStoreApplication.service.OrderService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/bookstore_user")
